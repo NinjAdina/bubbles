0d134c39eec2d8ff6381c8d0c49af0de441d6c56
NinjAdina <AdinaShuttari@Adinas-MacBook-Pro.local>
2016-02-19 23:55:44 -0500

    Just Keep Swimming!
    In sketch.js:
    Made 200 fish “swim” from right to left, and more fish can be added
    with mouse click.
    Added update and display function.
    Added Starting number of fish function.
    Under setup function added 200 fish.
    Under draw still included mouseIsPressed if statement.
    
    In bubble.js:
    Instead of radius added fish width and height.
    They move at a faster pace along the x: (0, -5).
    In the ‘if’ statement changed everything to this.x. Then, taking from
    Daily 3, had fish reappear from the right edge of the canvas (width):
    if (this.x < this.fishWidth * -1[this indicates that when the x = -20,
    or when the fish completely disappears off the canvas]) this.x =
    this.fishWidth + width[then make the fish reappear just off the edge of
    the canvas at 620.];

--- bubble.js
+++ bubble.js
@@ -1,22 +1,25 @@
-var Bubble = function (x, y) {
+var Fish = function (x, y) {
   this.x = x;
   this.y = y;
 };
 
-Bubble.prototype = {
+Fish.prototype = {
 
-  radius: 5,
+  fishWidth: 20,
+  fishHeight: 10,
 
   update: function() {
-    this.x += random(-1, 1);
-    this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+    this.x += random(0, -5); // this.x = this.x + random(0,2)
+    this.y += random(-1, 1);
+    if (this.x < this.fishWidth * -1) this.x =  this.fishWidth + width;
+    // if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);----- this made bubbles "fizz" and collect on the top.
+    // if (this.y < this.radius * -1) this.y = this.radius + height;---- this made bubbles move continuously from the top to reappear from the bottom.
   },
 
   display: function () {
     noStroke();
-    fill(102, 217, 255, 100);
-    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    fill(102, 100, 300, 100);
+    rect(this.x, this.y, this.fishWidth, this.fishHeight);
   }
 
 };
--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
-var bubbles = [];
+var fish = [];
+
+var updateAndDisplay = function(fishy) {
+  fishy.update();
+  fishy.display();
+};
+
+var addNumFish = function(totalFish) {
+  while(fish.length < totalFish) {
+    fish.push(new Fish(random(0, width), random(0, height)));
+  }
+};
 
 setup = function () {
   createCanvas(600, 600);
+  addNumFish(200);
   // your code goes here
 };
 
 draw = function () {
-  background(0);
   // your code goes here
+  background(0);
+
+  if (mouseIsPressed) fish.push(new Fish(mouseX, mouseY));
+
+  fish.forEach(updateAndDisplay);
+
 };
